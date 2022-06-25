d7f92a87e02b8ba67ee4f12070106d415292a8a6
Nishith Fernando <61730280+NFernando98@users.noreply.github.com>
2022-06-25 18:49:55 -0400

    add comment

--- astar.py
+++ astar.py
@@ -3,6 +3,9 @@ import pygame
 import math
 from queue import PriorityQueue
 
+# comment
+
+
 WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("A* Path Finding Algorithm")
